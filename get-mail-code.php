78a278c6f4d77b019823e003ea0316df04965ad8
yasu <yasu@tcp-net.ad.jp>
2014-09-02 14:56:36 +0900

    first commit

---
+++ get-mail-code.php
@@ -0,0 +1,28 @@
+<?php
+  /*
+    Plugin Name: Get Mail Code
+    Description: メールアドレスを ASCIIコード に変換し Javascript で表示する
+    Author: yasu@tcp
+    Version: 1.0
+  */
+ 
+function get_mail_code_shortcode($atts) {
+    extract(shortcode_atts(array(
+                                 'mail' => '',
+                                 ),$atts));
+    $mail_str = explode('@',$mail);
+    $local = str2ascii($mail_str[0].'@');
+    $domain = str2ascii($mail_str[1]);
+
+    $ret_str  = '<script type="text/javascript">var ma=String.fromCharCode(';
+    $ret_str .= $local;
+    $ret_str .= ')+String.fromCharCode(';
+    $ret_str .= $domain;
+    $ret_str .= ');';
+    $ret_str .= 'document.write(';
+    $ret_str .= "'<a href=\"mai'+'lto:'+ma+'\">'+ma+'</a>');";
+    $ret_str .= '</script>';
+    return $ret_str;
+}
+
+add_shortcode('get_mail_code','get_mail_code_shortcode');
